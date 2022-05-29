4550e2d05e4a9ceaed0aee06ef51744b2fd83f15
haider <haider.n@somaiya.edu>
2022-05-29 11:59:27 +0530

    first commit

---
+++ fib.js
@@ -0,0 +1,17 @@
+// memoization
+
+let counter = 0
+const fib = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+  console.log(counter);
+  counter++
+  if (n <= 2) return 1;
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n]
+}
+
+// console.log(fib(6));
+// console.log(fib(7));
+// console.log(fib(8));
+// console.log(fib(25));
+console.log(fib(50));
